f5485e5666fb802a201fabc3e2e7815aa96c3d41
OWEN\Owen <qiaokuanghua@gmail.com>
2019-10-31 23:07:26 -0400

    Added definition for switch commands

---
+++ EPS_STM8L/Debug/mppt.ls
@@ -0,0 +1,4 @@
+   1                     ; C Compiler for STM8 (COSMIC Software)
+   2                     ; Parser V4.10.2 - 02 Nov 2011
+   3                     ; Generator (Limited) V4.3.7 - 29 Nov 2011
+  34                     	end
--- EPS_STM8L/sw.h
+++ EPS_STM8L/sw.h
@@ -14,6 +14,40 @@
 #define SW_I2C_SPEED      	100000 /*!< I2C Speed */
 #define I2C_TIMEOUT         (uint32_t)0x3FFFF /*!< I2C Time out */
 
+// Command Byte Operation (used as Cmd parameter of sw_WriteCmdByte() function)
+#define V_CONT							0x01
+#define V_ONCE							0x02
+#define I_CONT							0x04
+#define I_ONCE							0x08
+#define VRANGE							0x10
+#define STATUS_RD						0x40
+
+// Extended Register Addresses (used as RegAddr parameter of sw_WriteExtCmdByte() function)
+#define ALERT_EN						0x01
+#define ALERT_TH						0x02
+#define CONTROL							0x03
+
+// ALERT_EN Register Operations (used as ExtCmd parameter of sw_WriteExtCmdByte() function)
+#define EN_ADC_OC1					0x01
+#define EN_ADC_OC4					0x02
+#define EN_HS_ALERT					0x04
+#define EN_OFF_ALERT				0x08
+#define CLEAR								0x10
+
+//	ALERT_TH Register Operations (sets the current threshhold for alert)
+
+// CONTROL Register Operations
+#define SWOFF								0x01 // may setting this bit to zero can turn the switch back on?
+
+//Status Byte Operations (gained by using sw_ReadStatus() function)
+#define ADC_OC							0x01
+#define ADC_ALERT						0x02
+#define HS_OC								0x04
+#define HS_ALERT						0x08
+#define OFF_STATUS					0x10
+#define OFF_ALERT						0x40
+
+
 #define setBit(reg, bit) (reg = reg | (1 << bit))
 #define clearBit(reg, bit) (reg = reg & ~(1 << bit))
 #define toggleBit(reg, bit) (reg = reg ^ (1 << bit))
